96af19c88499c53beb6362fe06000eb9fad6f0e0
Sebastian Ceffalotti <sebastian.ceffalotti@gmail.com>
2024-04-17 14:49:10 -0300

    Agregada la resolucion de los ejercicios del (2,7]

--- Guia 3/ejercicio_1.hs
+++ Guia 3/ejercicio_1.hs
@@ -1,15 +1,15 @@
 f :: Integer -> Integer
-f n | 1 = 8
-    | 4 = 131
-    | 16 = 16
+f n | n == 1 = 8
+    | n == 4 = 131
+    | n == 16 = 16
 
 g :: Integer -> Integer
-g n | 8 = 16
-    | 16 = 4
-    | 131 = 1
+g n | n == 8 = 16
+    | n == 16 = 4
+    | n == 131 = 1
 
 h :: Integer -> Integer
 h n = f(g n)
 
 k :: Integer -> Integer
-k n = g.f n
+k n = g(f n)
---
+++ Guia 3/ejercicio_7.hs
@@ -0,0 +1,6 @@
+absoluto :: Float -> Float
+absoluto x  | x < 0 = -x
+            | otherwise = x
+
+distanciaManhattan:: (Float, Float, Float) ->(Float, Float, Float) ->Float
+distanciaManhattan (x1,y1,z1) (x2,y2,z2) = absoluto(x1-x2) + absoluto(y1-y2) + absoluto(z1-z2)
